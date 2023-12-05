435abd884cccd10d252e6d5af421b47ee992b683
Chris <134155599+chrislaai@users.noreply.github.com>
2023-12-05 11:48:30 +0800

    feat: Clean up Serverless Modules
    In this case we are not going to use the Serverless Modules

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@ const { withStoreConfig } = require("./store-config")
 const store = require("./store.config.json")
 
 module.exports = withStoreConfig({
-  experimental: {
-    serverComponentsExternalPackages: [
-      "@medusajs/product",
-      "@medusajs/modules-sdk",
-    ],
-  },
   features: store.features,
   reactStrictMode: true,
   images: {
